5c051b1270567b9fd30d073e46449d8d89d54a25
Robin van Leeuwen <rvanleeuwen@jouwomgeving.nl>
2018-03-24 13:56:47 +0100

    Update mongo w/ questions, running mongo once w/ parameters should be enought to fill questions, users & populated answers

--- mongo.js
+++ mongo.js
@@ -1,44 +1,18 @@
-// load('./questions_txt.js');
-// load('./users.js');
+const questions = require('./questions_txt');
 const https = require('https');
 const MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
-const calcMin = require('lodash/fp/min');
+const ObjectId = require('mongodb').ObjectId;
 const calcMax = require('lodash/fp/max');
-var randomGeoPoints = require('./randomgeo');
-var userAmount = process.argv[2] || 500;
-var userRadius = process.argv[3] || 150000;
-var locations = randomGeoPoints(
+const randomGeoPoints = require('./randomgeo');
+const userAmount = process.argv[2] || 500;
+const userRadius = process.argv[3] || 150000;
+const locations = randomGeoPoints(
   { lat: 52.322046, lng: 4.956157 },
   userRadius,
   userAmount
 );
-var maxRange = ['5000', '10000', '20000', '50000', '100000', '150000'];
-var questionLength = 0;
-
-// Questions
-// questions.map(q => {
-//   db.questions.insert({
-//     // id: q.id,
-//     question: q.question,
-//     answer: q.answers,
-//   });
-// });
-
-// questions w/ answers
-// var users = db.users.find();
-// var questions = db.questions.find();
-// db.users.find().forEach(function(user) {
-//   db.questions.find().forEach(function(question) {
-//     db.answers.insert({
-//       user: user._id,
-//       question: question._id,
-//       q: question.question,
-//       answer:
-//         question.answer[Math.floor(Math.random() * question.answer.length)],
-//     });
-//   });
-// });
+const maxRange = ['5000', '10000', '20000', '50000', '100000', '150000'];
+const questionLength = 0;
 
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -69,6 +43,7 @@ https.get(`https://randomuser.me/api/?results=${userAmount}`, res => {
 function getUserData(data) {
   var users = [];
   var answers = [];
+  var questionList = [];
 
   for (var index = 0; index < data.results.length; index++) {
     users.push({
@@ -95,72 +70,91 @@ function getUserData(data) {
 
   MongoClient.connect('mongodb://localhost:27017/', function(err, db) {
     if (err) throw err;
-    db
-      .db('endless')
-      .collection('questions')
-      .count()
-      .then(function(qLength) {
-        fillUsers(qLength);
+
+    questions.forEach(q => {
+      questionList.push({
+        question: q.question,
+        answer: q.answers,
       });
-    function fillUsers(questionLength) {
-      db
-        .db('endless')
-        .collection('users')
-        .insertMany(users, function(err, res) {
-          if (err) throw err;
-          fillQuestions(res.ops);
-        });
-      function fillQuestions(users) {
+      if (questions.length === questionList.length) {
         db
           .db('endless')
-          .collection('users')
-          .createIndex({ location: '2dsphere' });
-
-        users.forEach(function(user) {
-          db
-            .db('endless')
-            .collection('questions')
-            .find()
-            .forEach((question, index) => {
-              answers.push({
-                user: user._id,
-                question: question._id,
-                q: question.question,
-                answer:
-                  question.answer[
-                    Math.floor(Math.random() * question.answer.length)
-                  ],
+          .collection('questions')
+          .insertMany(questionList, function(err, res) {
+            if (err) throw err;
+            db
+              .db('endless')
+              .collection('questions')
+              .count()
+              .then(function(qLength) {
+                fillUsers(qLength);
               });
-              if (users.length * questionLength === answers.length) {
+            function fillUsers(questionLength) {
+              db
+                .db('endless')
+                .collection('users')
+                .insertMany(users, function(err, res) {
+                  if (err) throw err;
+                  fillQuestions(res.ops);
+                });
+              function fillQuestions(users) {
                 db
                   .db('endless')
-                  .collection('answers')
-                  .insertMany(answers, function(err, res) {
-                    if (err) throw err;
-                    done();
-                  });
+                  .collection('users')
+                  .createIndex({ location: '2dsphere' });
+
+                users.forEach(function(user) {
+                  db
+                    .db('endless')
+                    .collection('questions')
+                    .find()
+                    .forEach((question, index) => {
+                      answers.push({
+                        user: user._id,
+                        question: question._id,
+                        q: question.question,
+                        answer:
+                          question.answer[
+                            Math.floor(Math.random() * question.answer.length)
+                          ],
+                      });
+                      if (users.length * questionLength === answers.length) {
+                        db
+                          .db('endless')
+                          .collection('answers')
+                          .insertMany(answers, function(err, res) {
+                            if (err) throw err;
+                            done();
+                          });
+                      }
+                    });
+                });
+                function done() {
+                  db
+                    .db('endless')
+                    .collection('users')
+                    .insertOne({
+                      name: 'Cees Ligtelijn',
+                      gender: 'male',
+                      username: 'cees',
+                      age: 20,
+                      _id: ObjectId('5aaa5ddcec8f2ebc9bfb496b'),
+                      minAge: 18,
+                      maxAge: 25,
+                      prefers: 'female',
+                      location: {
+                        type: 'Point',
+                        coordinates: [52.322125, 4.956157],
+                      },
+                      maxRange:
+                        maxRange[Math.floor(Math.random() * maxRange.length)],
+                    })
+                    .then(db.close());
+                }
               }
-            });
-        });
-        function done() {
-          db
-            .db('endless')
-            .collection('users')
-            .insertOne({
-              name: 'Cees Ligtelijn',
-              gender: 'male',
-              username: 'cees',
-              age: 20,
-              _id: ObjectId('5aaa5ddcec8f2ebc9bfb496b'),
-              minAge: 18,
-              maxAge: 25,
-              prefers: 'female',
-              location: { type: 'Point', coordinates: [52.322125, 4.956157] },
-              maxRange: maxRange[Math.floor(Math.random() * maxRange.length)],
-            })
-            .then(db.close());
-        }
+            }
+          });
       }
-    }
+    });
   });
 }
