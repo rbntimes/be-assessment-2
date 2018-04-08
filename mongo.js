69fcd52258af32cd1ba78de97fcf6d16dd524068
Robin van Leeuwen <rvanleeuwen@jouwomgeving.nl>
2018-04-08 23:34:10 +0200

    Devide some fun trivia questions over the dummy users

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ const randomGeoPoints = require('./randomgeo');
 const userAmount = process.argv[2] || 500;
 const userRadius = process.argv[3] || 150000;
 const locations = randomGeoPoints(
-  { lat: 52.322046, lng: 4.956157 },
+  { lat: 52.07925, lng: 5.122515 },
   userRadius,
   userAmount
 );
@@ -27,77 +27,118 @@ function minMaxAge() {
   };
 }
 
-// DOE NIET GIERIG MET DIE GET BABY DEEL UIT (bombadier - zwart licht)
-https.get(`https://randomuser.me/api/?results=${userAmount}`, res => {
-  res.setEncoding('utf8');
-  let body = '';
-  res.on('data', data => {
-    body += data;
-  });
-  res.on('end', () => {
-    body = JSON.parse(body);
-    getUserData(body);
-  });
-});
-
-function getUserData(data) {
-  var users = [];
-  var answers = [];
-  var questionList = [];
-
-  for (var index = 0; index < data.results.length; index++) {
-    users.push({
-      name: `${capitalize(data.results[index].name.first)} ${capitalize(
-        data.results[index].name.last
-      )}`,
-      gender: data.results[index].gender,
-      age: calcMax([Math.floor(Math.random() * 80), 18]),
-      minAge: minMaxAge()['min'],
-      maxAge: minMaxAge()['max'],
-      username: data.results[index].login.username,
-      password: data.results[index].login.password,
-      email: data.results[index].email,
-      picture: data.results[index].picture.large,
-      registered: data.results[index].registered,
-      nationality: data.results[index].nat,
-      location: {
-        type: 'Point',
-        coordinates: [locations[index].latitude, locations[index].longitude],
-      },
-      maxRange: maxRange[Math.floor(Math.random() * maxRange.length)],
+https.get(
+  'https://opentdb.com/api.php?amount=50&difficulty=easy&type=multiple',
+  res => {
+    res.setEncoding('utf8');
+    let body = '';
+    res.on('data', data => {
+      body += data;
+    });
+    res.on('end', () => {
+      getUsers(JSON.parse(body));
     });
   }
+);
 
-  MongoClient.connect('mongodb://localhost:27017/', function(err, db) {
-    if (err) throw err;
+function getUsers(apiQuestions) {
+  // DOE NIET GIERIG MET DIE GET BABY DEEL UIT (bombadier - zwart licht)
+  https.get(`https://randomuser.me/api/?results=${userAmount}`, res => {
+    res.setEncoding('utf8');
+    let body = '';
+    res.on('data', data => {
+      body += data;
+    });
+    res.on('end', () => {
+      body = JSON.parse(body);
+      getUserData(body);
+    });
+  });
 
-    questions.forEach(q => {
-      questionList.push({
-        question: q.question,
-        answer: q.answers,
+  function fillApiQuestions() {
+    list = [];
+    for (var i = 0; i < 5; i++) {
+      rand = Math.floor(Math.random() * apiQuestions.results.length);
+      apiQuestions.results[rand].incorrect_answers.push(
+        apiQuestions.results[rand].correct_answer
+      );
+      list.push({
+        question: apiQuestions.results[rand].question,
+        answers: apiQuestions.results[rand].incorrect_answers,
       });
-      if (questions.length === questionList.length) {
-        db
-          .db('endless')
-          .collection('questions')
-          .insertMany(questionList, function(err, res) {
-            if (err) throw err;
-            db
-              .db('endless')
-              .collection('questions')
-              .count()
-              .then(function(qLength) {
-                fillUsers(qLength);
-              });
-            function fillUsers(questionLength) {
+    }
+    return list;
+  }
+
+  function getUserData(data) {
+    var users = [];
+    var answers = [];
+    var questionList = [];
+
+    for (var index = 0; index < data.results.length; index++) {
+      users.push({
+        name: `${capitalize(data.results[index].name.first)} ${capitalize(
+          data.results[index].name.last
+        )}`,
+        gender: data.results[index].gender,
+        age: calcMax([Math.floor(Math.random() * 80), 18]),
+        minAge: minMaxAge()['min'],
+        maxAge: minMaxAge()['max'],
+        username: data.results[index].login.username,
+        password: data.results[index].login.password,
+        email: data.results[index].email,
+        picture: data.results[index].picture.large,
+        registered: data.results[index].registered,
+        nationality: data.results[index].nat,
+        location: {
+          type: 'Point',
+          coordinates: [locations[index].latitude, locations[index].longitude],
+        },
+        maxRange: maxRange[Math.floor(Math.random() * maxRange.length)],
+      });
+    }
+
+    MongoClient.connect('mongodb://localhost:27017/', function(err, db) {
+      if (err) throw err;
+
+      db
+        .db('endless')
+        .collection('users')
+        .insertMany(users, function(err, res) {
+          if (err) throw err;
+          users = res.ops;
+          questions.forEach((q, index) => {
+            questionList.push({
+              index,
+              user: users[Math.floor(Math.random() * users.length)]._id,
+              questionId: ObjectId(),
+              question: q.question,
+              answers: q.answers,
+            });
+          });
+          apiQuestions.results.forEach((q, index) => {
+            questionList.push({
+              index: questions.length + index,
+              user: users[Math.floor(Math.random() * users.length)]._id,
+              questionId: ObjectId(),
+              question: q.question,
+              answers: q.incorrect_answers,
+            });
+          });
+
+          db
+            .db('endless')
+            .collection('questions')
+            .insertMany(questionList, function(err, res) {
+              if (err) throw err;
               db
                 .db('endless')
-                .collection('users')
-                .insertMany(users, function(err, res) {
-                  if (err) throw err;
-                  fillQuestions(res.ops);
+                .collection('questions')
+                .count()
+                .then(function(qLength) {
+                  fillUsers(qLength);
                 });
-              function fillQuestions(users) {
+              function fillUsers(questionLength) {
                 db
                   .db('endless')
                   .collection('users')
@@ -111,11 +152,11 @@ function getUserData(data) {
                     .forEach((question, index) => {
                       answers.push({
                         user: user._id,
-                        question: question._id,
-                        q: question.question,
+                        questionId: question._id,
+                        question: question.question,
                         answer:
-                          question.answer[
-                            Math.floor(Math.random() * question.answer.length)
+                          question.answers[
+                            Math.floor(Math.random() * question.answers.length)
                           ],
                       });
                       if (users.length * questionLength === answers.length) {
@@ -130,31 +171,11 @@ function getUserData(data) {
                     });
                 });
                 function done() {
-                  db
-                    .db('endless')
-                    .collection('users')
-                    .insertOne({
-                      name: 'Cees Ligtelijn',
-                      gender: 'male',
-                      username: 'cees',
-                      age: 20,
-                      _id: ObjectId('5aaa5ddcec8f2ebc9bfb496b'),
-                      minAge: 18,
-                      maxAge: 25,
-                      prefers: 'female',
-                      location: {
-                        type: 'Point',
-                        coordinates: [52.322125, 4.956157],
-                      },
-                      maxRange:
-                        maxRange[Math.floor(Math.random() * maxRange.length)],
-                    })
-                    .then(db.close());
+                  db.close();
                 }
               }
-            }
-          });
-      }
+            });
+        });
     });
-  });
+  }
 }
